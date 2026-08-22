15c3f28e3487a27694369f25189321c2f174b152
Muskan Singh <219332542+muskan04s@users.noreply.github.com>
2026-08-22 16:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (100.00%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.c
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.c
@@ -1,14 +1,8 @@
 bool checkDivisibility(int n) {
-    int sum = 0;
-    int product =1;
-    int temp = n;
-     while(temp!=0){
-        int digits = temp % 10;
-        sum += digits;
-        product *= digits;
-        temp /= 10;
+    int sum = 0,product = 1;
+    for(int temp = n; temp > 0; temp /= 10){
+        sum += temp%10;
+        product *= temp%10;
      }
-
-     int total = sum + product;
-     return (n % total == 0);
+     return (n % (sum+product) == 0);
 }
