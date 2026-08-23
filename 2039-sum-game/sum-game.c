6318998b617603a3596dc031c71c7248ce7345de
Muskan Singh <219332542+muskan04s@users.noreply.github.com>
2026-08-23 16:34:30 +0530

    Time: 2 ms (57.14%) | Memory: 10.2 MB (71.43%) - LeetSync

--- 2039-sum-game/sum-game.c
+++ 2039-sum-game/sum-game.c
@@ -1,23 +1,16 @@
+#include <stdbool.h>
+#include <string.h>
+
 bool sumGame(char* num) {
-    int n = strlen(num);
-    int half = n/2;
-    int sum_diff = 0;
-    int q_diff = 0;
+    double diff = 0;
     
-    for(int i = 0; i < n; i++){
-        int sign = (i<half) ? 1 : -1;
-
-        if(num[i] == '?'){
-            q_diff += sign;
-        } else {
-            sum_diff += sign * (num[i] - '0');
+    // Two pointers moving inward: i from start, j from end
+    for (int i = 0, j = strlen(num) - 1; i < j; i++, j--) {
+        diff += (num[i] == '?') ? 4.5 : (num[i] - '0');
+        diff -= (num[j] == '?') ? 4.5 : (num[j] - '0');
     }
-}
-
-if ((q_diff % 2) != 0){
-    return true;
-}
-
-return (sum_diff + (q_diff/2)*9) != 0;
+    
+    // If net diff is non-zero, Alice can force a win
+    return diff != 0;
 }
 
